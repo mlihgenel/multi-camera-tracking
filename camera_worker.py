64bc7b25e123401a7f865d9d479dc8f60fa08366
Melih Genel <mlihgenel@gmail.com>
2025-08-25 10:10:38 +0300

    add try-catch blocks and logger

--- camera_worker.py
+++ camera_worker.py
@@ -1,19 +1,27 @@
 import cv2
-import numpy as np
 import time 
 from datetime import datetime
 from detect_motion import detect_motion
 from log import logger
 
-def camera_worker(cam_id, src, result_queue, stop_event, yolo_queue,skip=1):
-    cap = cv2.VideoCapture(src)
-    if not cap.isOpened():
-        print(f"[Cam{cam_id}] Kamera açılamadı")
-        logger.warning(f"Cam{cam_id} açılamadı")
-        return
-
+def camera_worker(cam_id, src, result_queue, stop_event, skip=1):
+    
+    try:
+        cap = cv2.VideoCapture(src)
+        if not cap.isOpened():
+            print(f"[Cam{cam_id}] Kamera açılamadı")
+            logger.warning(f"Cam{cam_id} açılamadı")
+            return
+        logger.info("Kamera/video başarılya başlatıldı")
+    except:
+         pass    
+    
     frame_id = 0
-    mask = cv2.createBackgroundSubtractorMOG2(250, 150, True)
+    try:
+        mask = cv2.createBackgroundSubtractorMOG2(250, 150, True)
+    except Exception as e:
+        logger.warning(f"Motion detection maskelemesinde hata: {e}")
+    
     count_frame = 0 
     
     while not stop_event.is_set(): # kontrol değişkeni 
@@ -26,20 +34,18 @@ def camera_worker(cam_id, src, result_queue, stop_event, yolo_queue,skip=1):
         try:
             frame_resized = cv2.resize(frame, (600,400))
         except:
-            pass 
+            logger.warning("Frame boyutlandırmasında hata oluştur") 
         
         if frame_id % skip == 0:
             fps = cap.get(cv2.CAP_PROP_FPS)
             count_frame += 1 
             motion, processed_frame = detect_motion(frame_resized, mask, count_frame)            
             processed_frame = cv2.putText(processed_frame, f"{fps} FPS", (10,30), cv2.FONT_HERSHEY_DUPLEX, 0.8, 255, 1)
-            result_queue.put((cam_id, frame_id, datetime.now().strftime("%d-%m-%Y %H:%M:%S"), processed_frame, motion))
+            result_queue.put((cam_id, frame_id, datetime.now().strftime("%d-%m-%Y %H:%M:%S"), frame_resized, motion))
             time.sleep(0.03)
             
-            yolo_queue.put((frame_resized, datetime.now().strftime("%d-%m-%Y %H:%M:%S"), motion))
         frame_id += 1
         
-
 
     cap.release()
     print(f"[Cam{cam_id}] Durduruldu.")
--- track.py
+++ track.py
@@ -1,25 +1,26 @@
-from ultralytics import YOLO
-import cv2
+from log import logger
 
-def yolo_worker(yolo_queue, stop_event):
+def yolo_track_frame(frame, model, tracker_config="botsort.yaml", classes=[0, 15, 16, 2, 7]):
 
-    model = YOLO("yolo11n.pt")
-    tracker = "botsort.yaml"
-
-    while not stop_event.is_set():
-        if not yolo_queue.empty():
-            frame, time, motion = yolo_queue.get()
-
-            if motion:
-                results = model.track(
-                    source=frame,  # ❌ burada timestamp değil frame gidecek
-                    tracker=tracker,
-                    classes=[0, 15, 16, 2, 7],  # insan, köpek, kedi, araba, kamyon
-                    iou=0.5,
-                    persist=True,
-                    show=False,
-                    verbose=False
-                )
-                results[0].plot()
+    try:
+        results = model.track(
+            source=frame,
+            tracker=tracker_config,
+            classes=classes,
+            iou=0.5,
+            persist=True,
+            show=False,
+            verbose=False
+        )
+        
+        if results and len(results) > 0 and results[0].boxes is not None:
+            annotated_frame = results[0].plot()
+            return annotated_frame
+        else:
+            return frame
+            
+    except Exception as e:
+        logger.error(f"YOLO tracking hatası: {e}")
+        return frame
             
 
